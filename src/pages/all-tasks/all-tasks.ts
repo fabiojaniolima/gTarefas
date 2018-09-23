09b59731cbc82dd32c2e1d03f96c70b43d78e52a
fabiojaniolima <fabiojaniolima@gmail.com>
2018-09-23 14:24:48 -0300

    Add alerta para exclusão de item

--- src/pages/all-tasks/all-tasks.ts
+++ src/pages/all-tasks/all-tasks.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController, ItemSliding, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-all-tasks',
@@ -7,9 +7,32 @@ import { NavController } from 'ionic-angular';
 })
 export class AllTasksPage {
 
-  constructor(public navCtrl: NavController) { }
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) { }
 
   openAddEditTaskPage(): void {
     this.navCtrl.push('AddEditTaskPage');
+  }
+
+  dialogDeleteTask(slidingItem: ItemSliding) {
+    const confirm = this.alertCtrl.create({
+      title: 'Deletar tarefa!',
+      message: '<strong>Atenção</strong>: Essa ação não tem como ser desfeita.',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            slidingItem.close();
+          }
+        },
+        {
+          text: 'Deletar',
+          handler: () => {
+            slidingItem.close();
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController, ItemSliding, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,10 +7,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) { }
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) { }
 
   openAddEditTaskPage(): void {
     this.navCtrl.push('AddEditTaskPage');
+  }
+
+  dialogDeleteTask(slidingItem: ItemSliding) {
+    const confirm = this.alertCtrl.create({
+      title: 'Deletar tarefa!',
+      message: '<strong>Atenção</strong>: Essa ação não tem como ser desfeita.',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            slidingItem.close();
+          }
+        },
+        {
+          text: 'Deletar',
+          handler: () => {
+            slidingItem.close();
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
 }
